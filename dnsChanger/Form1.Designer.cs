3b51f31b347f28168a1e8d97f54c6dec4d7de1ba
ali-moments <a.ham2334@gmai.com>
2025-07-18 15:04:24 +0330

    Add project files.

---
+++ dnsChanger/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace dnsChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adaptorsComboBox = new MaterialSkin.Controls.MaterialComboBox();
+            this.connectButton = new MaterialSkin.Controls.MaterialButton();
+            this.pingButton = new MaterialSkin.Controls.MaterialButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.dnsComboBox = new MaterialSkin.Controls.MaterialComboBox();
+            this.clearSwitch = new MaterialSkin.Controls.MaterialSwitch();
+            this.adaptorStatusLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.dnsPingLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // adaptorsComboBox
+            // 
+            this.adaptorsComboBox.AutoResize = false;
+            this.adaptorsComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.adaptorsComboBox.Depth = 0;
+            this.adaptorsComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.adaptorsComboBox.DropDownHeight = 174;
+            this.adaptorsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.adaptorsComboBox.DropDownWidth = 121;
+            this.adaptorsComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.adaptorsComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.adaptorsComboBox.FormattingEnabled = true;
+            this.adaptorsComboBox.IntegralHeight = false;
+            this.adaptorsComboBox.ItemHeight = 43;
+            this.adaptorsComboBox.Location = new System.Drawing.Point(117, 79);
+            this.adaptorsComboBox.MaxDropDownItems = 4;
+            this.adaptorsComboBox.MouseState = MaterialSkin.MouseState.OUT;
+            this.adaptorsComboBox.Name = "adaptorsComboBox";
+            this.adaptorsComboBox.Size = new System.Drawing.Size(243, 49);
+            this.adaptorsComboBox.StartIndex = 0;
+            this.adaptorsComboBox.TabIndex = 0;
+            this.adaptorsComboBox.SelectedIndexChanged += new System.EventHandler(this.adaptorsComboBox_SelectedIndexChanged);
+            // 
+            // connectButton
+            // 
+            this.connectButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.connectButton.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.connectButton.Depth = 0;
+            this.connectButton.HighEmphasis = true;
+            this.connectButton.Icon = null;
+            this.connectButton.Location = new System.Drawing.Point(24, 283);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.connectButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.connectButton.Name = "connectButton";
+            this.connectButton.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.connectButton.Size = new System.Drawing.Size(64, 36);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "SET";
+            this.connectButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.connectButton.UseAccentColor = false;
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // pingButton
+            // 
+            this.pingButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pingButton.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.pingButton.Depth = 0;
+            this.pingButton.HighEmphasis = true;
+            this.pingButton.Icon = null;
+            this.pingButton.Location = new System.Drawing.Point(407, 283);
+            this.pingButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.pingButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.pingButton.Name = "pingButton";
+            this.pingButton.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.pingButton.Size = new System.Drawing.Size(64, 36);
+            this.pingButton.TabIndex = 2;
+            this.pingButton.Text = "ping";
+            this.pingButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.pingButton.UseAccentColor = false;
+            this.pingButton.UseVisualStyleBackColor = true;
+            this.pingButton.Click += new System.EventHandler(this.pingButton_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(34, 96);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(57, 19);
+            this.materialLabel1.TabIndex = 4;
+            this.materialLabel1.Text = "Adaptor";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel2.Location = new System.Drawing.Point(45, 163);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(33, 19);
+            this.materialLabel2.TabIndex = 5;
+            this.materialLabel2.Text = "DNS";
+            // 
+            // dnsComboBox
+            // 
+            this.dnsComboBox.AutoResize = false;
+            this.dnsComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dnsComboBox.Depth = 0;
+            this.dnsComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.dnsComboBox.DropDownHeight = 174;
+            this.dnsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dnsComboBox.DropDownWidth = 121;
+            this.dnsComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.dnsComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.dnsComboBox.FormattingEnabled = true;
+            this.dnsComboBox.IntegralHeight = false;
+            this.dnsComboBox.ItemHeight = 43;
+            this.dnsComboBox.Location = new System.Drawing.Point(117, 148);
+            this.dnsComboBox.MaxDropDownItems = 4;
+            this.dnsComboBox.MouseState = MaterialSkin.MouseState.OUT;
+            this.dnsComboBox.Name = "dnsComboBox";
+            this.dnsComboBox.Size = new System.Drawing.Size(243, 49);
+            this.dnsComboBox.StartIndex = 0;
+            this.dnsComboBox.TabIndex = 6;
+            // 
+            // clearSwitch
+            // 
+            this.clearSwitch.AutoSize = true;
+            this.clearSwitch.Depth = 0;
+            this.clearSwitch.Location = new System.Drawing.Point(24, 227);
+            this.clearSwitch.Margin = new System.Windows.Forms.Padding(0);
+            this.clearSwitch.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.clearSwitch.MouseState = MaterialSkin.MouseState.HOVER;
+            this.clearSwitch.Name = "clearSwitch";
+            this.clearSwitch.Ripple = true;
+            this.clearSwitch.Size = new System.Drawing.Size(173, 37);
+            this.clearSwitch.TabIndex = 7;
+            this.clearSwitch.Text = "clear dns on exit";
+            this.clearSwitch.UseVisualStyleBackColor = true;
+            // 
+            // adaptorStatusLabel
+            // 
+            this.adaptorStatusLabel.AutoSize = true;
+            this.adaptorStatusLabel.Depth = 0;
+            this.adaptorStatusLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.adaptorStatusLabel.Location = new System.Drawing.Point(383, 96);
+            this.adaptorStatusLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.adaptorStatusLabel.Name = "adaptorStatusLabel";
+            this.adaptorStatusLabel.Size = new System.Drawing.Size(66, 19);
+            this.adaptorStatusLabel.TabIndex = 8;
+            this.adaptorStatusLabel.Text = "unknown";
+            // 
+            // dnsPingLabel
+            // 
+            this.dnsPingLabel.AutoSize = true;
+            this.dnsPingLabel.Depth = 0;
+            this.dnsPingLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dnsPingLabel.Location = new System.Drawing.Point(383, 163);
+            this.dnsPingLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.dnsPingLabel.Name = "dnsPingLabel";
+            this.dnsPingLabel.Size = new System.Drawing.Size(44, 19);
+            this.dnsPingLabel.TabIndex = 9;
+            this.dnsPingLabel.Text = "ping: -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 335);
+            this.Controls.Add(this.dnsPingLabel);
+            this.Controls.Add(this.adaptorStatusLabel);
+            this.Controls.Add(this.clearSwitch);
+            this.Controls.Add(this.dnsComboBox);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.pingButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.adaptorsComboBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DNS changer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialComboBox adaptorsComboBox;
+        private MaterialSkin.Controls.MaterialButton connectButton;
+        private MaterialSkin.Controls.MaterialButton pingButton;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialComboBox dnsComboBox;
+        private MaterialSkin.Controls.MaterialSwitch clearSwitch;
+        private MaterialSkin.Controls.MaterialLabel adaptorStatusLabel;
+        private MaterialSkin.Controls.MaterialLabel dnsPingLabel;
+    }
+}
+
---
+++ dnsChanger/Form1.cs
@@ -0,0 +1,235 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dnsChanger
+{
+    public partial class Form1 : MaterialForm
+    {
+        private Dictionary<string, (string primary, string secondary)> dnsServers;
+
+        private void RunNetshCommand(string arguments)
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "netsh",
+                Arguments = arguments,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (var process = System.Diagnostics.Process.Start(psi))
+            {
+                process.WaitForExit();
+                // Optional: read output or log errors
+                // string output = process.StandardOutput.ReadToEnd();
+                // string error = process.StandardError.ReadToEnd();
+            }
+        }
+
+
+        private void SetDns(string adapterName, string primary, string secondary)
+        {
+            try
+            {
+                // Clear existing DNS
+                RunNetshCommand($"interface ip set dns name=\"{adapterName}\" source=dhcp");
+
+                if (!string.IsNullOrWhiteSpace(primary))
+                    RunNetshCommand($"interface ip set dns name=\"{adapterName}\" static {primary}");
+
+                if (!string.IsNullOrWhiteSpace(secondary))
+                    RunNetshCommand($"interface ip add dns name=\"{adapterName}\" {secondary} index=2");
+
+                MessageBox.Show("DNS successfully updated.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to set DNS:\n{ex.Message}");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.FormClosing += Form1_FormClosing;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pingButton.Click += pingButton_Click;
+            adaptorsComboBox.SelectedIndexChanged += adaptorsComboBox_SelectedIndexChanged;
+
+            // Get all network interfaces
+            var adapters = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(ni =>
+                    ni.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                    ni.OperationalStatus == OperationalStatus.Up)
+                .ToList();
+
+            // Clear and populate the combo box
+            adaptorsComboBox.Items.Clear();
+            foreach (var adapter in adapters)
+            {
+                adaptorsComboBox.Items.Add(adapter.Name);
+            }
+
+            if (adaptorsComboBox.Items.Count > 0)
+            {
+                adaptorsComboBox.SelectedIndex = 0;
+            }
+
+            dnsServers = new Dictionary<string, (string primary, string secondary)>
+            {
+                { "Automatic (DHCP)", ("", "") },
+                { "Google", ("8.8.8.8", "8.8.4.4") },
+                { "Cloudflare", ("1.1.1.1", "1.0.0.1") },
+                { "Quad9", ("9.9.9.9", "149.112.112.112") },
+                { "OpenDNS", ("208.67.222.222", "208.67.220.220") },
+                { "AdGuard", ("94.140.14.14", "94.140.15.15") },
+                { "Shecan", ("178.22.122.100", "185.51.200.2") },
+                { "Electro DNS", ("78.157.42.100", "78.157.42.101") },
+                { "RadarGame", ("10.202.10.10", "10.202.10.11") },
+                { "403.online", ("10.202.10.202", "10.202.10.102") },
+            };
+
+            // Populate the combo box with names
+            dnsComboBox.Items.Clear();
+            foreach (var name in dnsServers.Keys)
+            {
+                dnsComboBox.Items.Add(name);
+            }
+
+            dnsComboBox.SelectedIndex = 0;
+
+        }
+
+        private void adaptorsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Optional: Show "Checking..." while working
+            adaptorStatusLabel.Text = "Checking...";
+            adaptorStatusLabel.ForeColor = Color.Gray;
+
+            // Ping a reliable address (e.g., Google DNS)
+            bool isOnline = false;
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    PingReply reply = ping.Send("8.8.8.8", 1000); // 1 second timeout
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        isOnline = true;
+                    }
+                }
+            }
+            catch
+            {
+                isOnline = false;
+            }
+
+            // Update label based on result
+            if (isOnline)
+            {
+                adaptorStatusLabel.Text = "Online";
+                adaptorStatusLabel.ForeColor = Color.Green;
+            }
+            else
+            {
+                adaptorStatusLabel.Text = "Offline";
+                adaptorStatusLabel.ForeColor = Color.Red;
+            }
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            string adapter = adaptorsComboBox.SelectedItem?.ToString();
+            string dnsName = dnsComboBox.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(adapter) || string.IsNullOrEmpty(dnsName))
+            {
+                MessageBox.Show("Please select both adapter and DNS.");
+                return;
+            }
+
+            var (primary, secondary) = dnsServers[dnsName];
+            SetDns(adapter, primary, secondary);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (clearSwitch.Checked)
+            {
+                string adapter = adaptorsComboBox.SelectedItem?.ToString();
+                if (!string.IsNullOrEmpty(adapter))
+                {
+                    try
+                    {
+                        // Reset DNS to automatic (DHCP)
+                        RunNetshCommand($"interface ip set dns name=\"{adapter}\" source=dhcp");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Failed to clear DNS:\n{ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void pingButton_Click(object sender, EventArgs e)
+        {
+            string dnsName = dnsComboBox.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(dnsName) || !dnsServers.ContainsKey(dnsName))
+            {
+                dnsPingLabel.Text = "Invalid DNS selection";
+                dnsPingLabel.ForeColor = Color.Gray;
+                return;
+            }
+
+            var (primary, _) = dnsServers[dnsName];
+            if (string.IsNullOrWhiteSpace(primary))
+            {
+                dnsPingLabel.Text = "No DNS IP to ping";
+                dnsPingLabel.ForeColor = Color.Gray;
+                return;
+            }
+
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    var reply = ping.Send(primary, 1000); // 1 sec timeout
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        dnsPingLabel.Text = $"Ping: {reply.RoundtripTime} ms";
+                        dnsPingLabel.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        dnsPingLabel.Text = "Unreachable";
+                        dnsPingLabel.ForeColor = Color.Red;
+                    }
+                }
+            }
+            catch
+            {
+                dnsPingLabel.Text = "Ping failed";
+                dnsPingLabel.ForeColor = Color.Red;
+            }
+        }
+    }
+}
